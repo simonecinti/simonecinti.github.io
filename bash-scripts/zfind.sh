1644b6cba072c316c6eceb8561a1d4e543ec253a
Simone Cinti <simone83.cinti@gmail.com>
2020-07-30 15:38:36 +0200

    fixes a typo in comments

--- bash-scripts/zfind.sh
+++ bash-scripts/zfind.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-# ----------------------------------------------------------------
+# --------------------------------------------------------------------
 # zfind.sh  -  a zip finder bash script util
-# ----------------------------------------------------------------
+# --------------------------------------------------------------------
 #
-# usage:  ./zfind.sh "filename-or-wildcard" "pattern-to-search" 
+# usage:	./zfind.sh "filename-or-wildcard" "pattern-to-search" 
 #
-# example: ./zfind.sh "*.jar" ".class"
-#         Gets lists all file names having ".class" inside the
-#         "*.jar" archives 
+# example:	./zfind.sh "*.jar" ".class"
+#         	to list all file names having ".class" inside the
+#         	"*.jar" archives 
 #          
 #
-# author: Simone Cinti - 2020-07-30
-# ----------------------------------------------------------------
+# author:	Simone Cinti - 2020-07-30
+# --------------------------------------------------------------------
 
 if [ "$#" -ne 2 ]; then
     echo 'zfind.sh - illegal number of parameters. Usage: ./zfind.sh "filename-or-wildcard" "pattern-to-search"'
